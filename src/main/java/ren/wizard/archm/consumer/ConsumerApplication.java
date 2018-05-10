129e3dff3fa5a100bbea7fbafbd1e6e9d4efd4e1
chnyangjie <chnyangjie@163.com>
2018-05-10 17:42:18 +0800

    add postgresql

--- src/main/java/ren/wizard/archm/consumer/ConsumerApplication.java
+++ src/main/java/ren/wizard/archm/consumer/ConsumerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author uyangjie
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableSwagger2
 public class ConsumerApplication {
 
     public static void main(String[] args) {
